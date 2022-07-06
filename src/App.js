39aab071d39f991047848effd6934d4b62aa093c
csd-31 <95088571+csd-31@users.noreply.github.com>
2022-07-06 14:01:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { Space, Typography, Layout } from 'antd'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className='app'>
+    <div className='navbar'> 
+    <Navbar/>
     </div>
+    
+    <div className='main'>
+      <Layout>
+        <div className='routes'>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+      <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+      <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
+      <Route path='/news' element={<News/>}/>
+    </Routes>
+    </div>
+    </Layout>
+     <div className='footer'>
+    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+        </Space>
+    </div> 
+    </div>
+    </div>
+    </BrowserRouter>
   );
 }
 
